ec5674f973d1a6fac35daf4df44e99a94772e713
Anthony Goodman <anthony@heyneighbor.us>
2025-06-21 00:03:21 -0400

    Update App.jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-/** @jsxImportSource react */
-import * as React from 'react';
-import { useState, useEffect } from 'react';
+// @ts-nocheck
+import React, { useState, useEffect } from 'react';
 import { 
   Users, 
   AlertTriangle, 
